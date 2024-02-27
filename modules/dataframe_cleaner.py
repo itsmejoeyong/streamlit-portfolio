baebd1543b547577b12d40d5545f70a428d9bda5
JoeYong01 <joeanselmyz@gmail.com>
2024-02-27 19:37:51 +0800

    added main.py & formatted modules/*

--- modules/dataframe_cleaner.py
+++ modules/dataframe_cleaner.py
@@ -2,8 +2,8 @@
 import re
 import pandas as pd
 
-class DataFrameCleaner():
-    
+
+class DataFrameCleaner:
     def clean_dataframe(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         cleans the dataframe using various functions
@@ -18,31 +18,33 @@ class DataFrameCleaner():
             self._format_columns,
             self._filter_negative_int,
             self._validate_date,
-            self._drop_dupes
+            self._drop_dupes,
         ]
-        
+
         for method in cleaning_methods:
             df = method(df)
-        
+
         return df
-    
+
     def _format_columns(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         converts CamelCase into snake_case using regex
 
         Args:
             column (str): column to be passed
-        
+
         Output:
             returns the cleaned column name
-        
+
         Example:
             - vendorID > vendor_id
             - TypeApheresisPlatelet > type_apheresis_platelet
         """
-        df.columns = [re.sub(r'(?<!^)([A-Z]+)', r'_\1', col).lower().strip() for col in df.columns]
+        df.columns = [
+            re.sub(r"(?<!^)([A-Z]+)", r"_\1", col).lower().strip() for col in df.columns
+        ]
         return df
-    
+
     def _filter_negative_int(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         filters any negative int
@@ -57,7 +59,7 @@ class DataFrameCleaner():
             if pd.api.types.is_integer_dtype(df[col]):
                 df[col] = df[col].mask(df[col] < 0)
         return df
-    
+
     def _validate_date(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         converts the column to datetime else NaT
@@ -70,9 +72,9 @@ class DataFrameCleaner():
         """
         for col in df.columns:
             if pd.api.types.is_datetime64_any_dtype(df[col]):
-                df[col] = pd.to_datetime(df[col], errors='coerce')
+                df[col] = pd.to_datetime(df[col], errors="coerce")
         return df
-    
+
     def _drop_dupes(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         drops duplicate rows from a dataframe
--- modules/dataframe_manager.py
+++ modules/dataframe_manager.py
@@ -3,63 +3,68 @@ from pathlib import Path
 
 import pandas as pd
 
-class DataFrameManager():
+
+class DataFrameManager:
     def __init__(self, path_to_df: str):
         self.file_path = path_to_df
         self.path = Path(path_to_df)
         self.name = self.path.name
         self.df = self._initialize_df()
-    
+
     def _initialize_df(self) -> pd.DataFrame:
         """
         reads a csv/parquet via URL/file
 
         Returns:
             pd.DataFrame: pandas dataframe
-        
+
         Raises:
             ValueError: If the file format is not supported
         """
         last_exception = None
-        readers = [
-            self.read_csv,
-            self.read_parquet
-        ]
-        
+        readers = [self.read_csv, self.read_parquet]
+
         for reader in readers:
             try:
                 return reader(self.file_path)
             except Exception as e:
                 last_exception = e
-            
-            raise ValueError(f"Failed to read the file as any supported format. Last error: {last_exception}")
-    
+
+        raise ValueError(
+            f"Failed to read the file as any supported format. Last error: {last_exception}"
+        )
+
     @staticmethod
     def read_csv(path: Path) -> pd.DataFrame:
         return pd.read_csv(path)
-    
+
     @staticmethod
     def read_parquet(path: Path) -> pd.DataFrame:
         return pd.read_parquet(path)
-    
-    
-    def write_df_to_file(self, output_path: str = None, file_type: str = 'csv', index: bool = False, compress: bool = False) -> None:
+
+    def write_df_to_file(
+        self,
+        output_path: str = None,
+        file_type: str = "csv",
+        index: bool = False,
+        compress: bool = False,
+    ) -> None:
         if output_path is None:
-            output_path = self.file_path.rsplit('.', 1)[0] + f"_modified.{file_type}"
-        
+            output_path = self.file_path.rsplit(".", 1)[0] + f"_modified.{file_type}"
+
         writer = {
-            'csv': self._write_csv,
-            'xlsx': self._write_xlsx,
-            'parquet': self._write_parquet
+            "csv": self._write_csv,
+            "xlsx": self._write_xlsx,
+            "parquet": self._write_parquet,
         }
-        
+
         if file_type in writer:
             writer[file_type](output_path, index, compress)
         else:
             raise ValueError("Supported types are csv, xlsx/excel & parquet")
-    
+
     def _write_csv(self, output, index, compress):
-        compression = 'gzip' if compress else None
+        compression = "gzip" if compress else None
         self.df.to_csv(output, index=index, compression=compression)
 
     def _write_xlsx(self, output, index, compress):
@@ -67,5 +72,5 @@ class DataFrameManager():
         self.df.to_excel(output, index=index)
 
     def _write_parquet(self, output, index, compress):
-        compression = 'gzip' if compress else None
+        compression = "gzip" if compress else None
         self.df.to_parquet(output, index=index, compression=compression)
