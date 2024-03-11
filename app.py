7e2689987a41738d9c43c8b5c646387c256e9edc
JoeYong01 <joeanselmyz@gmail.com>
2024-03-11 17:32:34 +0800

    updated naming & added typehints

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
 PAGE_NAMES_TO_FUNCS: dict = {"homepage": homepage}
 
 # ----- SIDEBAR SELECTBOX -----
-page_selector = st.sidebar.selectbox("Select a page", PAGE_NAMES_TO_FUNCS.keys())
-PAGE_NAMES_TO_FUNCS[page_selector]()
+page_selected = st.sidebar.selectbox("Select a page", PAGE_NAMES_TO_FUNCS.keys())
+PAGE_NAMES_TO_FUNCS[page_selected]()
