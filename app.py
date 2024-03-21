3ac1cf8b255dd1182a201b5596ed3e50d2a31dbc
JoeYong01 <joeanselmyz@gmail.com>
2024-03-21 18:22:00 +0800

    updated age grouping, validated cohorts retention

--- app.py
+++ app.py
@@ -306,223 +306,226 @@ with st.expander("__Donation Metrics By Hospital__"):
     st.markdown("> For some reason in streamlit there's an issue with year groupings as you can see, therefore the buckets we see should be 1 year less")
 
 st.divider()
-# granular_average_months_between_donations_query = """
-# WITH next_visit AS(
-# SELECT
-#     birth_date,
-#     visit_date::DATE as visit_date,
-#     LEAD(visit_date::DATE) OVER(PARTITION BY donor_id ORDER BY visit_date) AS next_visit_date
-# FROM
-#     ds_data_granular
-# ),
+granular_average_months_between_donations_query = """
+WITH next_visit AS(
+SELECT
+    birth_date,
+    visit_date::DATE as visit_date,
+    LEAD(visit_date::DATE) OVER(PARTITION BY donor_id ORDER BY visit_date) AS next_visit_date
+FROM
+    ds_data_granular
+),
 
-# age_on_visit AS(
-# SELECT
-#     ABS(visit_date - next_visit_date) AS days_between_visit,
-#     EXTRACT(YEAR FROM visit_date) - birth_date AS age_on_visit
-# FROM    
-#     next_visit 
-# ),
+age_on_visit AS(
+SELECT
+    ABS(visit_date - next_visit_date) AS days_between_visit,
+    EXTRACT(YEAR FROM visit_date) - birth_date AS age_on_visit
+FROM    
+    next_visit 
+),
 
-# age_group AS(
-#     SELECT
-#     *,
-#     CASE
-#         WHEN age_on_visit < 20 THEN '<20'
-#         WHEN age_on_visit BETWEEN 20 AND 29 THEN '20-29'
-#         WHEN age_on_visit BETWEEN 30 AND 39 THEN '30-39'
-#         WHEN age_on_visit BETWEEN 40 AND 49 THEN '40-49'
-#         WHEN age_on_visit BETWEEN 50 AND 59 THEN '50-59'
-#         WHEN age_on_visit BETWEEN 60 AND 69 THEN '60-69'
-#         WHEN age_on_visit BETWEEN 70 AND 79 THEN '70-79'
-#         WHEN age_on_visit > 80 THEN '80+'
-#         END AS age_group,
-#     CASE
-#         WHEN age_on_visit < 20 THEN 1
-#         WHEN age_on_visit BETWEEN 20 AND 29 THEN 2
-#         WHEN age_on_visit BETWEEN 30 AND 39 THEN 3
-#         WHEN age_on_visit BETWEEN 40 AND 49 THEN 4
-#         WHEN age_on_visit BETWEEN 50 AND 59 THEN 5
-#         WHEN age_on_visit BETWEEN 60 AND 69 THEN 6
-#         WHEN age_on_visit BETWEEN 70 AND 79 THEN 7
-#         WHEN age_on_visit > 80 THEN 8
-#         END AS age_group_order
-# FROM
-#     age_on_visit
-# )
+age_group AS(
+    SELECT
+    *,
+    CASE
+        WHEN age_on_visit BETWEEN 20 AND 29 THEN '20-29'
+        WHEN age_on_visit BETWEEN 30 AND 39 THEN '30-39'
+        WHEN age_on_visit BETWEEN 40 AND 49 THEN '40-49'
+        WHEN age_on_visit BETWEEN 50 AND 59 THEN '50-59'
+        WHEN age_on_visit BETWEEN 60 AND 69 THEN '60-69'
+        WHEN age_on_visit BETWEEN 70 AND 79 THEN '70-79'
+        WHEN age_on_visit > 80 THEN '80+'
+        END AS age_group,
+    CASE
+        WHEN age_on_visit < 20 THEN 1
+        WHEN age_on_visit BETWEEN 20 AND 29 THEN 2
+        WHEN age_on_visit BETWEEN 30 AND 39 THEN 3
+        WHEN age_on_visit BETWEEN 40 AND 49 THEN 4
+        WHEN age_on_visit BETWEEN 50 AND 59 THEN 5
+        WHEN age_on_visit BETWEEN 60 AND 69 THEN 6
+        WHEN age_on_visit BETWEEN 70 AND 79 THEN 7
+        WHEN age_on_visit > 80 THEN 8
+        END AS age_group_order
+FROM
+    age_on_visit
+WHERE
+    age_group IS NOT NULL
+)
 
-# SELECT
-#     age_group,
-#     FLOOR(AVG(days_between_visit) / 30) AS average_months_between_visits
-# FROM
-#     age_group
-# WHERE
-#     age_group IS NOT NULL
-# GROUP BY
-#     age_group,
-#     age_group_order
-# ORDER BY
-#     age_group_order
-# """
-# granular_average_months_between_visits_table = con.execute(granular_average_months_between_donations_query).df()
-# st.dataframe(granular_average_months_between_visits_table)
+SELECT
+    age_group,
+    FLOOR(AVG(days_between_visit) / 30) AS average_months_between_visits
+FROM
+    age_group
+WHERE
+    age_group IS NOT NULL
+GROUP BY
+    age_group,
+    age_group_order
+ORDER BY
+    age_group_order
+"""
+granular_average_months_between_visits_table = con.execute(granular_average_months_between_donations_query).df()
+st.dataframe(granular_average_months_between_visits_table)
 
-# # NOTE: churn definition: users who never donate blood once every 2 years
-# granular_average_months_before_churn_query_v2 = """
-# WITH next_visit AS(
-# SELECT
-#     donor_id,
-#     birth_date,
-#     visit_date::DATE as visit_date,
-#     LEAD(visit_date::DATE) OVER(PARTITION BY donor_id ORDER BY visit_date) AS next_visit_date
-# FROM
-#     ds_data_granular
-# ),
+# NOTE: churn definition: users who never donate blood once every 2 years
+granular_average_months_before_churn_query_v2 = """
+WITH next_visit AS(
+SELECT
+    donor_id,
+    birth_date,
+    visit_date::DATE as visit_date,
+    LEAD(visit_date::DATE) OVER(PARTITION BY donor_id ORDER BY visit_date) AS next_visit_date
+FROM
+    ds_data_granular
+),
 
-# age_on_visit AS(
-# SELECT
-#     donor_id,
-#     visit_date,
-#     next_visit_date,
-#     ABS(visit_date - next_visit_date) AS days_between_visit,
-#     EXTRACT(YEAR FROM visit_date) - birth_date AS age_on_visit
-# FROM    
-#     next_visit 
-# ),
+age_on_visit AS(
+SELECT
+    donor_id,
+    visit_date,
+    next_visit_date,
+    ABS(visit_date - next_visit_date) AS days_between_visit,
+    EXTRACT(YEAR FROM visit_date) - birth_date AS age_on_visit
+FROM    
+    next_visit 
+),
 
-# age_group AS(
-#     SELECT
-#     *,
-#     CASE
-#         WHEN age_on_visit < 20 THEN '<20'
-#         WHEN age_on_visit BETWEEN 20 AND 29 THEN '20-29'
-#         WHEN age_on_visit BETWEEN 30 AND 39 THEN '30-39'
-#         WHEN age_on_visit BETWEEN 40 AND 49 THEN '40-49'
-#         WHEN age_on_visit BETWEEN 50 AND 59 THEN '50-59'
-#         WHEN age_on_visit BETWEEN 60 AND 69 THEN '60-69'
-#         WHEN age_on_visit BETWEEN 70 AND 79 THEN '70-79'
-#         WHEN age_on_visit > 80 THEN '80+'
-#         END AS age_group,
-#     CASE
-#         WHEN age_on_visit < 20 THEN 1
-#         WHEN age_on_visit BETWEEN 20 AND 29 THEN 2
-#         WHEN age_on_visit BETWEEN 30 AND 39 THEN 3
-#         WHEN age_on_visit BETWEEN 40 AND 49 THEN 4
-#         WHEN age_on_visit BETWEEN 50 AND 59 THEN 5
-#         WHEN age_on_visit BETWEEN 60 AND 69 THEN 6
-#         WHEN age_on_visit BETWEEN 70 AND 79 THEN 7
-#         WHEN age_on_visit > 80 THEN 8
-#         END AS age_group_order
-# FROM
-#     age_on_visit
-# ),
+age_group AS(
+    SELECT
+    *,
+    CASE
+        WHEN age_on_visit BETWEEN 20 AND 29 THEN '20-29'
+        WHEN age_on_visit BETWEEN 30 AND 39 THEN '30-39'
+        WHEN age_on_visit BETWEEN 40 AND 49 THEN '40-49'
+        WHEN age_on_visit BETWEEN 50 AND 59 THEN '50-59'
+        WHEN age_on_visit BETWEEN 60 AND 69 THEN '60-69'
+        WHEN age_on_visit BETWEEN 70 AND 79 THEN '70-79'
+        WHEN age_on_visit > 80 THEN '80+'
+        END AS age_group,
+    CASE
+        WHEN age_on_visit < 20 THEN 1
+        WHEN age_on_visit BETWEEN 20 AND 29 THEN 2
+        WHEN age_on_visit BETWEEN 30 AND 39 THEN 3
+        WHEN age_on_visit BETWEEN 40 AND 49 THEN 4
+        WHEN age_on_visit BETWEEN 50 AND 59 THEN 5
+        WHEN age_on_visit BETWEEN 60 AND 69 THEN 6
+        WHEN age_on_visit BETWEEN 70 AND 79 THEN 7
+        WHEN age_on_visit > 80 THEN 8
+        END AS age_group_order
+FROM
+    age_on_visit
+WHERE
+    age_group IS NOT NULL
+),
 
-# rolling_sum_of_total_visits AS(
-# SELECT
-#     *,
-#     SUM(days_between_visit) OVER(PARTITION BY donor_id ORDER BY visit_date) AS rolling_total_days_between_visit,
-# FROM
-#     age_group
-# ),
+rolling_sum_of_total_visits AS(
+SELECT
+    *,
+    SUM(days_between_visit) OVER(PARTITION BY donor_id ORDER BY visit_date) AS rolling_total_days_between_visit,
+FROM
+    age_group
+),
 
-# days_before_churn AS(
-# SELECT
-#     *,
-#     CONCAT(EXTRACT(YEAR FROM visit_date) + 2, '-12-31')::DATE - visit_date AS days_before_next_churn
-# FROM
-#     rolling_sum_of_total_visits
-# ),
+days_before_churn AS(
+SELECT
+    *,
+    CONCAT(EXTRACT(YEAR FROM visit_date) + 2, '-12-31')::DATE - visit_date AS days_before_next_churn
+FROM
+    rolling_sum_of_total_visits
+),
 
-# churns AS(
-# SELECT
-#     *,
-#     IF(days_between_visit > days_before_next_churn, 1, 0) AS is_churn
-# FROM
-#     days_before_churn
-# WHERE
-#     is_churn = 1
-# )
+churns AS(
+SELECT
+    *,
+    IF(days_between_visit > days_before_next_churn, 1, 0) AS is_churn
+FROM
+    days_before_churn
+WHERE
+    is_churn = 1
+)
 
-# SELECT
-#     age_group,
-#     FLOOR(AVG(rolling_total_days_between_visit / 30)) AS average_months_to_churn
-# FROM
-#     churns
-# GROUP BY
-#     age_group,
-#     age_group_order
-# ORDER BY
-#     age_group_order
-# """
-# granular_average_months_before_churn_table_v2 = con.execute(granular_average_months_before_churn_query_v2).df()
-# st.dataframe(granular_average_months_before_churn_table_v2)
+SELECT
+    age_group,
+    FLOOR(AVG(rolling_total_days_between_visit / 30)) AS average_months_to_churn
+FROM
+    churns
+GROUP BY
+    age_group,
+    age_group_order
+ORDER BY
+    age_group_order
+"""
+granular_average_months_before_churn_table_v2 = con.execute(granular_average_months_before_churn_query_v2).df()
+st.dataframe(granular_average_months_before_churn_table_v2)
 
-# granular_average_donations_by_age_group_query = """
-# WITH age_on_visit AS(
-#     SELECT
-#     donor_id,
-#     visit_date,
-#     EXTRACT(YEAR FROM visit_date) - birth_date AS age_on_visit
-# FROM
-#     ds_data_granular
-# ),
+granular_average_donations_by_age_group_query = """
+WITH age_on_visit AS(
+    SELECT
+    donor_id,
+    visit_date,
+    EXTRACT(YEAR FROM visit_date) - birth_date AS age_on_visit
+FROM
+    ds_data_granular
+),
 
-# age_group AS(
-# SELECT
-#     *,
-#     CASE
-#         WHEN age_on_visit < 20 THEN '<20'
-#         WHEN age_on_visit BETWEEN 20 AND 29 THEN '20-29'
-#         WHEN age_on_visit BETWEEN 30 AND 39 THEN '30-39'
-#         WHEN age_on_visit BETWEEN 40 AND 49 THEN '40-49'
-#         WHEN age_on_visit BETWEEN 50 AND 59 THEN '50-59'
-#         WHEN age_on_visit BETWEEN 60 AND 69 THEN '60-69'
-#         WHEN age_on_visit BETWEEN 70 AND 79 THEN '70-79'
-#         WHEN age_on_visit > 80 THEN '80+'
-#         END AS age_group,
-#     CASE
-#         WHEN age_on_visit < 20 THEN 1
-#         WHEN age_on_visit BETWEEN 20 AND 29 THEN 2
-#         WHEN age_on_visit BETWEEN 30 AND 39 THEN 3
-#         WHEN age_on_visit BETWEEN 40 AND 49 THEN 4
-#         WHEN age_on_visit BETWEEN 50 AND 59 THEN 5
-#         WHEN age_on_visit BETWEEN 60 AND 69 THEN 6
-#         WHEN age_on_visit BETWEEN 70 AND 79 THEN 7
-#         WHEN age_on_visit > 80 THEN 8
-#         END AS age_group_order
-# FROM
-#     age_on_visit
-# ),
+age_group AS(
+SELECT
+    *,
+    CASE
+        WHEN age_on_visit BETWEEN 20 AND 29 THEN '20-29'
+        WHEN age_on_visit BETWEEN 30 AND 39 THEN '30-39'
+        WHEN age_on_visit BETWEEN 40 AND 49 THEN '40-49'
+        WHEN age_on_visit BETWEEN 50 AND 59 THEN '50-59'
+        WHEN age_on_visit BETWEEN 60 AND 69 THEN '60-69'
+        WHEN age_on_visit BETWEEN 70 AND 79 THEN '70-79'
+        WHEN age_on_visit > 80 THEN '80+'
+        END AS age_group,
+    CASE
+        WHEN age_on_visit < 20 THEN 1
+        WHEN age_on_visit BETWEEN 20 AND 29 THEN 2
+        WHEN age_on_visit BETWEEN 30 AND 39 THEN 3
+        WHEN age_on_visit BETWEEN 40 AND 49 THEN 4
+        WHEN age_on_visit BETWEEN 50 AND 59 THEN 5
+        WHEN age_on_visit BETWEEN 60 AND 69 THEN 6
+        WHEN age_on_visit BETWEEN 70 AND 79 THEN 7
+        WHEN age_on_visit > 80 THEN 8
+        END AS age_group_order
+FROM
+    age_on_visit
+WHERE
+    age_group IS NOT NULL
+),
 
-# n_donations_by_age_group_and_donor AS(
-# SELECT
-#     donor_id,
-#     age_group,
-#     age_group_order,
-#     COUNT(*) AS n_donations
-# FROM
-#     age_group
-# WHERE
-#     age_group IS NOT NULL
-# GROUP BY
-#     donor_id,
-#     age_group,
-#     age_group_order
-# )
+n_donations_by_age_group_and_donor AS(
+SELECT
+    donor_id,
+    age_group,
+    age_group_order,
+    COUNT(*) AS n_donations
+FROM
+    age_group
+WHERE
+    age_group IS NOT NULL
+GROUP BY
+    donor_id,
+    age_group,
+    age_group_order
+)
 
-# SELECT
-#     age_group,
-#     ROUND(AVG(n_donations), 2) AS avg_donations
-# FROM
-#     n_donations_by_age_group_and_donor
-# GROUP BY
-#     age_group,
-#     age_group_order
-# ORDER BY
-#     age_group_order
-# """
-# granular_average_donations_by_age_group_table = con.execute(granular_average_donations_by_age_group_query).df()
-# st.dataframe(granular_average_donations_by_age_group_table)
+SELECT
+    age_group,
+    ROUND(AVG(n_donations), 2) AS avg_donations
+FROM
+    n_donations_by_age_group_and_donor
+GROUP BY
+    age_group,
+    age_group_order
+ORDER BY
+    age_group_order
+"""
+granular_average_donations_by_age_group_table = con.execute(granular_average_donations_by_age_group_query).df()
+st.dataframe(granular_average_donations_by_age_group_table)
 
 granular_cohorts_query = """
 WITH first_year_donation AS(
@@ -555,7 +558,6 @@ SELECT
     first_donation_year,
     donation_year,
     CASE
-        WHEN age_on_visit < 20 THEN '<20'
         WHEN age_on_visit BETWEEN 20 AND 29 THEN '20-29'
         WHEN age_on_visit BETWEEN 30 AND 39 THEN '30-39'
         WHEN age_on_visit BETWEEN 40 AND 49 THEN '40-49'
@@ -577,6 +579,8 @@ SELECT
     COUNT(DISTINCT donor_id) AS n_donors
 FROM
     yearly_donations AS yd
+WHERE
+    age_group IS NOT NULL
 GROUP BY
     first_donation_year,
     donation_year,
@@ -588,7 +592,6 @@ initial_cohort_size AS(
 SELECT
     first_donation_year,
     CASE
-        WHEN age_on_visit < 20 THEN '<20'
         WHEN age_on_visit BETWEEN 20 AND 29 THEN '20-29'
         WHEN age_on_visit BETWEEN 30 AND 39 THEN '30-39'
         WHEN age_on_visit BETWEEN 40 AND 49 THEN '40-49'
@@ -610,6 +613,8 @@ SELECT
     COUNT(DISTINCT donor_id) AS initial_donors
 FROM
     first_year_donation
+WHERE
+    age_group IS NOT NULL
 GROUP BY
     first_donation_year,
     age_group,
@@ -624,27 +629,22 @@ SELECT
     c.age_group_order,
     c.n_donors,
     ic.initial_donors,
-    -- Calculate retention as the percentage of initial donors who donated in each subsequent year
-    ROUND((CAST(c.n_donors AS FLOAT) / CAST(ic.initial_donors AS FLOAT)) * 100, 2) AS retention_rate
+    ROUND((c.n_donors / ic.initial_donors) * 100, 2) AS retention_rate
 FROM
-    cohorts c
+    cohorts AS c
 JOIN
-    initial_cohort_size ic
-ON
-    c.first_donation_year = ic.first_donation_year
-    AND c.age_group = ic.age_group
--- Ensure we only look at cohorts for years after the first donation year to avoid the impossible scenarios
+    initial_cohort_size AS ic
+    USING(first_donation_year, age_group)
 WHERE
     c.donation_year >= c.first_donation_year
 ),
 
--- Now, calculate the retention for each nth year since the first donation, for each age group
 retention_by_nth_year AS (
 SELECT
     age_group,
     age_group_order,
-    donation_year - first_donation_year + 1 AS nth_year,  -- +1 to adjust for the first year being year 1
-    AVG(retention_rate) AS average_retention_rate
+    donation_year - first_donation_year + 1 AS nth_year,
+    ROUND(AVG(retention_rate), 2) AS average_retention_rate
 FROM
     cohort_retention
 WHERE
@@ -657,6 +657,7 @@ ORDER BY
     age_group_order,
     nth_year
 )
+
 SELECT
     age_group,
     nth_year,
